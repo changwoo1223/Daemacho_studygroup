e2dc48228942e86cfb4ef938d339601edb68a6d0
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-22 18:40:43 +0900

    _2884

---
+++ changwoo1223/_2884/Main.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int H, M;
+	scanf("%d %d", &H, &M);
+	if (H >= 1) {
+		if (M >= 45) {
+			M = M - 45;
+		}
+		else {
+			H--;
+			M = M + 15;
+		}
+	}
+	else {
+		if (M >= 45) {
+			M = M - 45;
+		}
+		else {
+			H = 23;
+			M = M + 15;
+		}
+	}
+	printf("%d %d", H, M);
+	return 0;
+}
