8c0dd6ef08e99804b3ac61e686a9ae1790381346
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-22 19:16:05 +0900

    _2525

---
+++ changwoo1223/_2525/Main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int H, M, T, n;
+	scanf("%d %d\n%d", &H, &M, &T);
+	M = M + T;
+	n = M / 60;
+	if (M >= 60) {
+		M = M - (n*60);
+		H = H + n;
+	}
+	if (H >= 24) {
+		H = H - 24;
+	}
+	printf("%d %d", H, M);
+	return 0;
+}
