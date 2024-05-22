76426b0fda724fac76a26ca19d3fbffca9f954f3
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-22 19:57:47 +0900

    _2480

---
+++ changwoo1223/_2480/Main.c
@@ -0,0 +1,30 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int num1, num2, num3, highNum, reward = 0;
+	scanf("%d %d %d", &num1, &num2, &num3);
+	if (num1 == num2 && num2 == num3) {
+		reward = 10000 + (num1 * 1000);
+	}
+	else if (num1 == num2 || num2 == num3 ){
+		reward = 1000 + (num2 * 100);
+	}
+	else if (num1 == num3) {
+		reward = 1000 + (num1 * 100);
+	}
+	else {
+		if (num1 >= num2 && num1 >= num3) {
+			highNum = num1;
+		}
+		else if (num2 >= num1 && num2 >= num3) {
+			highNum = num2;
+		}
+		else if (num3 >= num1 && num3 >= num2) {
+			highNum = num3;
+		}
+		reward = highNum * 100;
+	}
+	printf("%d", reward);
+	return 0;
+}
