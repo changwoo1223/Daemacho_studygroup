491083622c3aed8a79ea02c5e6f1d2fdd5b208cf
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-22 20:42:03 +0900

    _10171

---
+++ changwoo1223/_10171/Main.c
@@ -0,0 +1,10 @@
+﻿#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+    printf("\\    /\\\n");
+    printf(" )  ( ')\n");
+    printf("(  /  )\n");
+    printf(" \\(__)|\n");
+    return 0;
+}
