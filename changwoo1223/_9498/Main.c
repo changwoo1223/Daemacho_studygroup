67b706b17ef13779318194529a2885b0d714e493
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 16:09:25 +0900

    _9498

---
+++ changwoo1223/_9498/Main.c
@@ -0,0 +1,22 @@
+﻿#include <stdio.h>
+
+int main() {
+    int point;
+    scanf("%d", &point);
+    if (point <= 100 && point >= 90) {
+        printf("A");
+    }
+    else if (point <= 89 && point >= 80) {
+        printf("B");
+    }
+    else if (point <= 79 && point >= 70) {
+        printf("C");
+    }
+    else if (point <= 69 && point >= 60) {
+        printf("D");
+    }
+    else {
+        printf("F");
+    }
+    return 0;
+}
