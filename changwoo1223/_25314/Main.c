233d2a073c7c0331e7d7edf8e4f0cdfcfd878ebf
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 16:36:20 +0900

    _25314

---
+++ changwoo1223/_25314/Main.c
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main() {
+    int N, output;
+
+    scanf("%d", &N);
+    output = N / 4;
+    for (int i = 1; i <= output; i++) {
+        printf("long ");
+    }
+    printf("int");
+    return 0;
+}
