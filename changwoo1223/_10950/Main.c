6ffa9cbd7a4341f44e5a64a45e827aa2017c3d19
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 15:18:42 +0900

    _10950

---
+++ changwoo1223/_10950/Main.c
@@ -0,0 +1,12 @@
+﻿#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int count, num1, num2;
+	scanf("%d", &count);
+	for (int i = 1; i <= count; i++) {
+		scanf("%d %d", &num1, &num2);
+		printf("%d\n", num1 + num2);
+	}
+	return 0;
+}
