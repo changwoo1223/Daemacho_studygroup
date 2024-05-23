afdf5045f8fed1e8d6a5cdf697190f68126bccf2
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 15:43:39 +0900

    _25304

---
+++ changwoo1223/_25304/Main.c
@@ -0,0 +1,19 @@
+﻿#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int X, N, sum = 0;
+	scanf("%d\n %d", &X, &N);
+	for (int i = 1; i <= N; i++) {
+		int a, b;
+		scanf("%d %d", &a, &b);
+		sum += a * b;
+	}
+	if (sum == X) {
+		printf("Yes");
+	}
+	else{
+		printf("No");
+	}
+	return 0;
+}
