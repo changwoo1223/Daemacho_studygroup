e96c4f5b88cae597b31b2f91587fedbade193ced
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 15:11:06 +0900

    _2739

---
+++ changwoo1223/2739/Main.c
@@ -0,0 +1,11 @@
+﻿#include<stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main() {
+	int num;
+	scanf("%d", &num);
+	for(int i = 1; i <= 9; i++) {
+		printf("%d * %d = %d\n", num, i, num * i);
+	}
+	return 0;
+}
