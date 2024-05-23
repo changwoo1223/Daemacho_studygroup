db44b57a67a639eeeb164892710ffc874cc6b60e
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 18:03:17 +0900

    _2439

---
+++ changwoo1223/_2439/Main.c
@@ -0,0 +1,19 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int N;
+    scanf("%d", &N);
+    
+    for (int i = 1; i < N+1; i++) {
+        for (int j = N - i; j > 0; j--) {
+            printf(" ");
+        }
+        for (int k = i; k > 0; k--) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
