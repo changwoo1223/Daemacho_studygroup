ecf3e9f94734a9610c2a98c12a7d78c4e68c29a8
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 16:04:53 +0900

    _11382

---
+++ changwoo1223/_11382/Main.c
@@ -0,0 +1,11 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+    double A, B, C;
+    scanf("%lf %lf %lf", &A, &B, &C);
+    printf("%.lf", A + B + C);
+
+    return 0;
+}
