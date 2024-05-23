a23ff726a6241dd484cf2ee9e736e8334c9e2a34
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 15:56:01 +0900

    _18108

---
+++ changwoo1223/18108/Main.c
@@ -0,0 +1,12 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+    int year1, year2;
+
+    scanf("%d", &year1);
+    year2 = year1 - 543;
+    printf("%d", year2);
+    return 0;
+}
