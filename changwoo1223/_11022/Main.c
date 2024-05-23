f95ef22026e4d1de769263ca4b94748f30aa1f72
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 17:08:12 +0900

    _11022

---
+++ changwoo1223/_11022/Main.c
@@ -0,0 +1,13 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+    int T, A, B, x = 1;
+    scanf("%d", &T);
+    for (int i = 1; i <= T; i++) {
+        scanf("%d %d", &A, &B);
+        printf("Case #%d: %d + %d = %d\n", x, A, B, A + B);
+        x++;
+    }
+    return 0;
+}
