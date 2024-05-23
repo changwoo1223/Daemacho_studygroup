4cdd76ee7497ccbfd7d4a75674555a6701d38074
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-23 15:54:37 +0900

    _10869

---
+++ changwoo1223/_10869/Main.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main()
+{
+    int A, B;
+    scanf("%d %d", &A, &B);
+    printf("%d\n", A + B);
+    printf("%d\n", A - B);
+    printf("%d\n", A * B);
+    printf("%d\n", A / B);
+    printf("%d\n", A % B);
+
+    return 0;
+}
