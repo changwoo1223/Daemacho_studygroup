1d9a80a713870179cd02b6c36805cfd335ae5334
changwoo1223 <167665877+changwoo1223@users.noreply.github.com>
2024-05-30 17:05:34 +0900

    _11720

---
+++ changwoo1223/_11720/Main.c
@@ -0,0 +1,14 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	int N, sum = 0;
+	char num[100];
+	scanf("%d", &N);
+	scanf("%s", num);
+	for (int i = 0; i < N; i++) {
+		sum += (int)num[i] - '0';
+	}
+	printf("%d", sum);
+	return 0;
+}
